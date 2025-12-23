470e806ef4594a6a4e04764b4a6dc1a186ae659e
Dev Luquinha <lucasemmanuelrodrigues737@gmail.com>
2025-12-23 14:58:48 -0300

    FEAT: Create the LicensesController

---
+++ LicenseGuard/Controllers/LicensesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LicenseGuard.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LicensesController : ControllerBase
+    {
+        [HttpPost("validate")]
+        public IActionResult ValidateLicense()
+        {
+            return Ok("License is valid.");
+        }
+    }
+}
